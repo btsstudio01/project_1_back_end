881120e5878df3357ae95b49249cb646c62b1e51
usman-90 <siddiquiusman328@gmail.com>
2024-01-24 18:46:22 +0500

    changes to push on vercel

--- middleware/JWT.js
+++ middleware/JWT.js
@@ -17,12 +17,12 @@ function verifyToken(req, res, next) {
     }
 
     jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ message: 'Invalid token' });
-        }
+      if (err) {
+          return res.status(401).json({ message: 'Invalid token' });
+      }
 
-        req.user = decoded;
-        next();
+      req.user = decoded;
+      next();
     });
 }
 
